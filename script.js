99e9aa3abe7e68eee8877e5f80e0234e78c4603a
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-12 12:13:15 -0400

    better script

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggleNav = () => {
+    document.getElementById("main-nav-items").classList.toggle("hidden");
+}
+
+const toggleSolarSystem = () => {
+
+}
+
+const toggleSun = () => {
+
+}
+
+const toggleMars = () => {
+
+}
+
+const toggleEarth = () => {
+    
+}
+
+window.onload = () => {
+    document.getElementById("nav-toggle").onclick = toggleNav;
+}
