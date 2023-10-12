987514a3cdc0790c42ae21e8f2fa188311c26b44
Spencer Goldfine <goldfis@email.sc.edu>
2023-10-12 12:21:34 -0400

    nav bar script commit

--- script.js
+++ script.js
@@ -2,20 +2,32 @@ const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
 
-const toggleSolarSystem = () => {
-
-}
+// const toggleSolarSystem = () => {
+//     document.getElementById("nav2").classList.toggle("hidden");
+//     document.getElementById("nav1").classList.add("hidden");
+//     document.getElementById("nav3").classList.add("hidden");
+//     document.getElementById("nav4").classList.add("hidden");
+// }
 
 const toggleSun = () => {
-
+    document.getElementById("nav2").classList.toggle("hidden");
+    document.getElementById("nav1").classList.add("hidden");
+    document.getElementById("nav3").classList.add("hidden");
+    document.getElementById("nav4").classList.add("hidden");
 }
 
 const toggleMars = () => {
-
+    document.getElementById("nav3").classList.toggle("hidden");
+    document.getElementById("nav1").classList.add("hidden");
+    document.getElementById("nav2").classList.add("hidden");
+    document.getElementById("nav4").classList.add("hidden");
 }
 
 const toggleEarth = () => {
-    
+    document.getElementById("nav4").classList.toggle("hidden");
+    document.getElementById("nav1").classList.add("hidden");
+    document.getElementById("nav2").classList.add("hidden");
+    document.getElementById("nav3").classList.add("hidden");
 }
 
 window.onload = () => {
